01b2075864813c9a8a06d44ff5462a8f2e6588b5
Lagutina Elizaveta <below8585@mail.ru>
2023-03-06 08:40:57 +0300

    Задачи с семинара

--- 003/Program.cs
+++ 003/Program.cs
@@ -1,11 +1,23 @@
 ﻿// Выведение квадрата чисел
-Console.Write("Введите число: ");
-int a = int.Parse(Console.ReadLine());
-int y = 1;
-while (y <= a)
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+int m = N; // m = -10
+if (N < 0)
 {
-    double y = Math.Pow(y, 2);
-    d = double y;
-    y++;
+    i = N; //i = -10
+    m = -N;// m = 10
+    N = -1; // N = -1
+    
+
 }
-Console.Write($"{a} -> {d}, ");
+int[] result = new int[m];
+for (int j = 0; i <= N; i++)
+{
+    result[j] = i * i;
+    j++;
+}
+for (int j = 0; j < m; j++)
+{
+    Console.Write($"{result[j]} ");
+}
